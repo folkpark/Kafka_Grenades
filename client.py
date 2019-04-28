96f047cee0a969106d192a9a0a34d91760bf6398
folkpark <parkerfolkman@gmail.com>
2019-04-28 16:58:47 -0600

    Producing test

--- client.py
+++ client.py
@@ -10,4 +10,5 @@ producer = KafkaProducer(bootstrap_servers=['3.95.28.49:9092'])
 for i in range(10):
     msg = "Testing %s" %(str(i))
     producer.send("test", b'msg')
+    print(producer.send('foobar', 'some_message_bytes').get(timeout=30))
     print("Sending %s"%msg)
