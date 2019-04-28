3bcd54e49cc01d59af9e2bb672189595506f2ae6
folkpark <parkerfolkman@gmail.com>
2019-04-28 16:28:36 -0600

    Producing test

--- client.py
+++ client.py
@@ -5,8 +5,9 @@ from kafka import KafkaConsumer, KafkaProducer
 #                          AUTO_OFFSET_RESET_CONFIG = 'earliest')
 
 
-producer = KafkaProducer(bootstrap_servers='ec2-3-95-28-49.compute-1.amazonaws.com:9092')
+producer = KafkaProducer(bootstrap_servers='3.95.28.49:9092')
 
 for i in range(10):
     msg = "Testing %s" %(str(i))
     producer.send("test", b'msg')
+    print("Sending %s"%msg)
