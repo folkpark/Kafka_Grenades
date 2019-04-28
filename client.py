fe5fc8897639d50fc24a120e7e7cc5588feae4e2
folkpark <parkerfolkman@gmail.com>
2019-04-28 17:32:05 -0600

    Producing test

--- client.py
+++ client.py
@@ -2,16 +2,11 @@ from kafka import KafkaConsumer, KafkaProducer
 import threading
 import time
 
-# consumer = KafkaConsumer('test',
-#                          bootstrap_servers='3.95.28.49:9092',
-#                          AUTO_OFFSET_RESET_CONFIG = 'earliest')
-
-
 def producer():
     producer = KafkaProducer(bootstrap_servers=['3.95.28.49:9092'])
     while True:
         for i in range(5):
-            msg = "Testing %s" %(str(i))
+            msg = "Producing %s" %(str(i))
             msg = msg.encode('utf-8')
             producer.send('test', msg).get(timeout=30)
             print("Sending %s"%msg)
@@ -25,8 +20,8 @@ def consumer():
                              bootstrap_servers=['3.95.28.49:9092'])
     # Should be infinite loop
     for messages in consumer:
-        message = message.value.decode("utf-8")
-        print(message)
+        message = messages.value.decode("utf-8")
+        print("I just consumed: %s" %message)
 
 
 
