eba7b8188bb58bdcb8633652180ddbf46e4e6b8d
stryder03 <stryder03@users.noreply.github.com>
2019-04-28 14:54:46 -0600

    updated file structure

--- GrenadeGame.py
+++ GrenadeGame.py
@@ -2,6 +2,9 @@ import Node
 from typing import List
 import threading
 from kafka import KafkaConsumer, KafkaProducer
+import time
+import random
+
 
 class GrenadeGame:
 
@@ -13,6 +16,7 @@ class GrenadeGame:
 class GameSetup:
 
     player_list = []
+
     def __init__(self, client_list, server_list, game_topic, connect_topic):
         self.player_total = len(client_list)
         self.client_setup(client_list)
@@ -32,12 +36,51 @@ class GameSetup:
             for message in find_players:
                 self.player_list.append(message.value.decode('utf8'))
             if len(self.player_list) > 1:
-                player_topics_list.append(Ka)
+                player_topics_list.append()
             else:
                 print('%s players are not enough' % len(self.player_list))
-
 
 
 class GameThread(threading.Thread):
 
     def __init__(self, threadID, ):
+
+
+
+
+
+
+
+
+class Grenade:
+
+    spoon_depressed: bool
+
+    def __init__(self):
+        self.pin_inserted = True
+        self.spoon_depressed = True
+        self.fuse_length = Grenade.fuse()
+        self.fuse_time = None
+
+    def pull_pin(self):
+        self.pin_inserted = False
+
+    def cook_grenade(self):
+        self.spoon_depressed = True
+        self.fuse_time = time.time() + self.fuse_length
+
+    def grenade_throw(self, velocity, angle, position):
+        pass
+
+    @staticmethod
+    def fuse():
+        fuse = random.random() * 5
+        if fuse < 3:
+            return 3
+        else:
+            return fuse
+
+
+
+
+
--- Node.py
+++ Node.py
@@ -1,6 +1,5 @@
-import time
 from kafka import KafkaConsumer, KafkaProducer
-import random
+import GrenadeGame
 
 
 class Node:
@@ -10,7 +9,7 @@ class Node:
         self.id = client_id
         self.role = role
         if role == 'client':
-            self.grenade = Grenade()
+            self.grenade = GrenadeGame.Grenade()
         self.timeout = timeout
         self.broker_addr = broker_addr
         self.producer = KafkaProducer(bootstrap_servers=self.broker_addr)
@@ -30,30 +29,3 @@ class Node:
         self.position = pos_rep
 
 
-class Grenade:
-
-    spoon_depressed: bool
-
-    def __init__(self):
-        self.pin_inserted = True
-        self.spoon_depressed = True
-        self.fuse_length = Grenade.fuse()
-        self.fuse_time = None
-
-    def pull_pin(self):
-        self.pin_inserted = False
-
-    def cook_grenade(self):
-        self.spoon_depressed = True
-        self.fuse_time = time.time() + self.fuse_length
-
-    def grenade_throw(self, velocity, angle, position):
-        pass
-
-    @staticmethod
-    def fuse():
-        fuse = random.random()*5
-        if fuse < 3:
-            return 3
-        else:
-            return fuse
